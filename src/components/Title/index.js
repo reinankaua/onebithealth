d480b89739f80909f99d28cda27d0ea08d502951
Reinan Kauã <contato.reinankaua@gmail.com>
2021-10-18 19:19:13 -0300

    master

---
+++ src/components/Title/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { 
+    View, 
+    Text 
+} from 'react-native'
+import styles from './style'
+
+export default function Title() {
+    return(
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>
+                ONEBITHEALTH
+            </Text>
+        </View>        
+    );
+
+}
